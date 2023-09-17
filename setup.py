f5d1bfb2023cf549943b140246e512f6f7838d57
cuire <81014305+cuire@users.noreply.github.com>
2023-09-17 13:42:17 +0500

    Put npmnuke ignore in home dir

--- setup.py
+++ setup.py
@@ -1,6 +1,25 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 from npmnuke import __version__
+
+
+def put_npmnukeignore_in_home_directory() -> None:
+    try:
+        with open(".npmnukeignore-template", "r") as f:
+            template = f.read()
+
+        ignore_file = Path.home() / ".npmnukeignore"
+        if not ignore_file.exists():
+            with open(ignore_file, "w") as f:
+                f.write(template)
+
+            # make file hidden
+
+    except Exception as e:
+        pass
+
 
 setup(
     name="npmnuke",
@@ -12,3 +31,5 @@ setup(
     packages=find_packages(),
     python_requires=">=3.10",
 )
+
+put_npmnukeignore_in_home_directory()
